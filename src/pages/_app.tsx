05a6f21bcec5857116f42d1df2c44fda426b77fa
Rafael Korz <rafael.korz@gmail.com>
2021-02-25 16:54:25 -0300

    Dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,24 @@
+import { ThemeProvider } from 'next-themes';
 import '../styles/global.css';
 import { ChallengesProvider } from '../contexts/ChallengesContext';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <ChallengesProvider>
-      <Component {...pageProps} />
-    </ChallengesProvider>
+    <ThemeProvider>
+      <ChallengesProvider>
+        <Component {...pageProps} />
+      </ChallengesProvider>
+    </ThemeProvider>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,49 @@ import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import styles from '../styles/pages/Home.module.css';
+import Switch from '@material-ui/core/Switch';
+
+import { useTheme } from "next-themes";
 
 import Head from 'next/head';
 import { ChallengeBox } from "../components/ChallengeBox";
 import { CountdownProvider } from "../contexts/CountdownContext";
+import { useState } from "react";
 
 export default function Home() {
+  const { theme, setTheme } = useTheme();
+  const [state, setState] = useState(false);
+  const [picture, setPicture] = useState('/icons/sunny.svg');
+
+  function handleChange() {
+    setState(!state);
+    if (state) {
+      setTheme('light');
+      setPicture('/icons/sunny.svg');
+    } else {
+      setTheme('dark');
+      setPicture('/icons/night.svg');
+    }
+  }
+
   return (
     <div className={styles.container}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          alignItems: 'center',
+          marginBottom: 10          
+        }}
+      >
+        <Switch
+          color={'primary'}
+          onChange={handleChange}
+          inputProps={{ 'aria-label': 'secondary checkbox' }}
+        />
+        <img style={{height: '1.5rem', marginLeft: '1rem'}} src={picture} alt="sunny"/>
+      </div>
+
       <Head>
         <link rel="shortcut icon" href="favicon.png" type="image/png" />
 
